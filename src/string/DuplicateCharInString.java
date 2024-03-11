ec8802120942ebd43b8273e19030357f331068d9
Suman0293 <suman@gmail.com>
2024-03-12 00:27:52 +0530

    ok

---
+++ src/string/DuplicateCharInString.java
@@ -0,0 +1,25 @@
+package string;
+
+public class DuplicateCharInString {
+    public static void main(String[] args) {
+
+        String name = "Suman kumari Bhardwaj";
+        name = name.toLowerCase();
+        int count = 0;
+
+        char[] ch = name.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < ch.length; j++) {
+                if (ch[i] == ch[j] && ch[i] != ' ') {
+                    count++;
+                    ch[j] = '0';
+                }
+            }
+            if (count > 1 && ch[i] != '0') {
+                System.out.println(ch[i]);
+            }
+        }
+    }
+}
