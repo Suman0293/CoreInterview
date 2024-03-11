3ee3d6fcfd7d6be96b8b29fcc2ab7dc84682f4b1
Suman0293 <suman@gmail.com>
2024-03-12 00:14:48 +0530

    ok

---
+++ src/string/AdditionOfIntegerPresentInString.java
@@ -0,0 +1,19 @@
+package string;
+
+public class AdditionOfIntegerPresentInString {
+    public static void main(String[] args) {
+        String[] name = {"Neeraj1234"};
+        int count = 0;
+        for (String s : name) {
+            char[] ch = s.toCharArray();
+            for (int i = 0; i < ch.length; i++) {
+                if (Character.isDigit(ch[i])) {
+                    count += Integer.parseInt(String.valueOf(ch[i]));
+
+                }
+            }
+
+        }
+        System.out.println(count);
+    }
+}
