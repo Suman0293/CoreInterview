4a37bef8a1d692c96d6c5980c6987458c634cd8e
Suman0293 <suman@gmail.com>
2024-03-12 14:01:53 +0530

    ok

---
+++ src/string/EqualOperator_EqualMethod.java
@@ -0,0 +1,26 @@
+package string;
+
+public class EqualOperator_EqualMethod {
+
+    public static void main(String[] args) {
+
+        String name1 = "Suman";
+        String name2 = "Suman";
+        String name3 = "Bhardwaj";
+        String name4 = new String("Suman");
+        String name5 = new String("Suman");
+        String name6 = new String("Bhardwaj");
+
+        System.out.println(name1 == name2);
+        System.out.println(name1 == name3);
+        System.out.println(name1 == name4);
+        System.out.println(name1 == name6);
+
+        System.out.println(name1.equals(name2));
+        System.out.println(name1.equals(name3));
+        System.out.println(name1.equals(name4));
+        System.out.println(name1.equals(name6));
+
+    }
+
+}
